0ce9b37f89043099c2fcc2eb59756580b3486a73
Matheus Lanca <matheus.7910@hotmail.com>
2019-08-31 19:41:02 -0300

    Adicionando tela de cadastro e login - Não finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroLoginComponent } from './cadastro-login/cadastro-login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CadastroLoginComponent },
+  { path: 'signUp', component: CadastroLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroLoginComponent } from './cadastro-login/cadastro-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroLoginComponent
   ],
   imports: [
     BrowserModule,
