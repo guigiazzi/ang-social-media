4c5ffa771e6aa0514585ee7b4c1d3fd73c2495ab
Gabriel Netto <biel_netto@hotmail.com>
2019-09-01 13:30:51 -0300

    Adicionando tela de postagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroLoginComponent } from './cadastro-login/cadastro-login.component';
+import { PostagensComponent } from './postagens/postagens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroLoginComponent
+    CadastroLoginComponent,
+    PostagensComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/postagens/postagens.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postagens',
+  templateUrl: './postagens.component.html',
+  styleUrls: ['./postagens.component.css']
+})
+
+export class PostagensComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
