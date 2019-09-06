e3b8d5ac0d0d5e60331c26f55043f3a2b6ccfbd3
Gabriel Netto <biel_netto@hotmail.com>
2019-09-06 18:26:54 -0300

    Adding Header Component

---
+++ src/app/shared/header-toolbar/header-toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-toolbar',
+  templateUrl: 'header-toolbar.component.html',
+  styleUrls: ['./header-toolbar.component.css']
+})
+
+export class HeaderToolbarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
