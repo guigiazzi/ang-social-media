1d28f81e1aab9ae84a010f707f0699fe9f3828b4
Matheus Lanca <matheus.7910@hotmail.com>
2019-09-07 17:32:06 -0300

    Ajustando configurações para heroku

--- server.js
+++ server.js
@@ -12,5 +12,14 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/ang-social-media/index.html'));
 });
 
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
