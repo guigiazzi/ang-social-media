66db7a8ca6154ac836d9c53f5d9af7e1bc868398
Gabriel Netto <biel_netto@hotmail.com>
2019-09-11 00:40:27 -0300

    Corrigindo listarDadosUsuario

--- src/app/postagens/dados-usuarios/dados.component.ts
+++ src/app/postagens/dados-usuarios/dados.component.ts
@@ -23,15 +23,18 @@ export class DadosComponent implements OnInit {
   retornaDadosUsuarios(user) {
     this.appservice.retornarDadosUsuario(user)
       .subscribe(user => {
-        user.birthDate = user.birthDate.substr(0,10);
-        user.birthDate = new Date(user.birthDate.replace(/-/g, '\/'));
-        user.birthDate =  ('0' + user.birthDate.getDate()).substr(-2) + '/'
-        + ('0' + (user.birthDate.getMonth() + 1)).substr(-2) + '/' + user.birthDate.getFullYear();
-
-        user.careerDate = user.careerDate.substr(0,10);
-        user.careerDate = new Date(user.careerDate.replace(/-/g, '\/'));
-        user.careerDate = ('0' + user.careerDate.getDate()).substr(-2) + '/'
-        + ('/' + (user.careerDate.getMonth() + 1)).substr(-2) + '/' + user.careerDate.getFullYear();
+        if(user.birthDate){
+          user.birthDate = user.birthDate.substr(0,10);
+          user.birthDate = new Date(user.birthDate.replace(/-/g, '\/'));
+          user.birthDate =  ('0' + user.birthDate.getDate()).substr(-2) + '/'
+          + ('0' + (user.birthDate.getMonth() + 1)).substr(-2) + '/' + user.birthDate.getFullYear();
+        }
+        if(user.careerDate){
+          user.careerDate = user.careerDate.substr(0,10);
+          user.careerDate = new Date(user.careerDate.replace(/-/g, '\/'));
+          user.careerDate = ('0' + user.careerDate.getDate()).substr(-2) + '/'
+          + ('/' + (user.careerDate.getMonth() + 1)).substr(-2) + '/' + user.careerDate.getFullYear();
+        }
         this.usuario = user;
       });
       this.topics = ['1','2','3','4'];
