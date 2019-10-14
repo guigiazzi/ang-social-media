cbfc68fb99234944c08659aba0ef0766532ec28a
Matheus Lança <matheus.7910@hotmail.com>
2019-10-14 10:59:47 -0300

    Ajustando tela de amizade

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -4,6 +4,7 @@ import { AppService } from '../app.service';
 import { SessionService } from '../shared/sessionService/session.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-friends',
@@ -16,7 +17,8 @@ export class FriendsComponent implements OnInit {
   public usuario: Professional = {} as Professional;
   public showSpinner = false;
   public haveUserImg = false;
-  public imageFile;
+  public imageFile: File;
+  public userDefault = '../../assets/images/user-icon.jpg';
 
   constructor(
     private router: Router,
@@ -24,6 +26,7 @@ export class FriendsComponent implements OnInit {
     private snackbar: MatSnackBar,
     private sessionService: SessionService,
     private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
@@ -31,43 +34,35 @@ export class FriendsComponent implements OnInit {
     this.userLoggedId = this.sessionService.getUserLogged();
     this.usuario.professionalID = this.route.snapshot.paramMap.get('id');
     this.appservice.getListFriends(this.usuario.professionalID)
-    .subscribe(res => {
-      this.friends = res;
-      console.log(this.friends);
-      this.showSpinner = false;
-    },
-    err => {
-      console.log(err);
-      this.snackbar.open('Ocorreu um erro ao carregar a lista de Amigos!', 'Dismiss', {
-        duration: 4000,
-        panelClass: ['error-snackbar']
-      });
-      this.showSpinner = false;
-    })
-    this.loadImage(this.friends);
+      .subscribe(res => {
+        this.friends = res;
+        console.log(this.friends);
+        this.showSpinner = false;
+        this.loadImage(this.friends);
+      },
+        err => {
+          console.log(err);
+          this.snackbar.open('Ocorreu um erro ao carregar a lista de Amigos!', 'Dismiss', {
+            duration: 4000,
+            panelClass: ['error-snackbar']
+          });
+          this.showSpinner = false;
+        })
   }
+
   loadImage(friends: Professional[]) {
-    const imgName = 'userImg.jpeg'
     friends.forEach(friend => {
       if (friend.profileImage !== null) {
-        const imageBlob = this.dataURItoBlob(friend.profileImage);
-        this.imageFile = new File([imageBlob], imgName, { type: 'image/jpeg' });
+        this.haveUserImg = true;
+        this.imageFile = friend.profileImage;
       }
+      this.haveUserImg = false;
     });
   }
-  dataURItoBlob(dataURI) {
-    const byteString = window.atob(dataURI);
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const int8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-      int8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([int8Array], { type: 'image/jpeg' });    
-    return blob;
- }
- goToUserProfile(id:String){
-   console.log(id);
-   this.router.navigate([`friends`, id]);
- }
- 
+
+  goToUserProfile(id: String) {
+    console.log(id);
+    this.router.navigate([`friends`, id]);
+  }
+
 }
